953e706f2b459de96b8c0cd92547b816b6850d76
OneCityInStorm <75725136+OneCityInStorm@users.noreply.github.com>
2021-09-08 14:31:00 +0800

    Add files via upload

---
+++ cloud-api-commons/src/main/java/com/atguigu/springcloud/entities/Payment.java
@@ -0,0 +1,36 @@
+package com.atguigu.springcloud.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+/**
+ * @Author 风雪壹城
+ * Date Create in 2021/8/10 9:54
+ * Description:
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Payment implements Serializable {
+    private Long id;
+    private String serial;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getSerial() {
+        return serial;
+    }
+
+    public void setSerial(String serial) {
+        this.serial = serial;
+    }
+}
